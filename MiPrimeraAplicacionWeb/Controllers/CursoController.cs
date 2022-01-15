98b6adf54759fb39f9d52ed30b6d48f202663873
LegionMata <Mata@LAPTOP-0MNSQMLN>
2022-01-15 18:46:04 -0300

    Add project files.

---
+++ MiPrimeraAplicacionWeb/Controllers/CursoController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MiPrimeraAplicacionWeb.Controllers
+{
+    public class CursoController : Controller
+    {
+        // GET: Curso
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string mensaje()
+        {
+            return "Bienvenido al Curso APS.NET  MVC";
+        }
+
+        public string saludo(string nombre)
+        {
+            return "Hola como estas : "+ nombre;
+        }
+
+        public string saludoCompleto(string nombre, string apellido)
+        {
+            return "Hola como estas : " + nombre +" "+ apellido;
+        }
+
+    }
+}
